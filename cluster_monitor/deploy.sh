31df97af9ad917564a533ce79899ce3b2d07ff2b
NagsaiPeddeti <nagsai.peddeti@gmail.com>
2022-04-30 21:11:13 +0530

    updated changes

--- cluster_monitor/deploy.sh
+++ cluster_monitor/deploy.sh
@@ -1,4 +1,4 @@
-export PROMETHEUS_CONFIG_NAME=prometheus.yml
+export PROMETHEUS_CONFIG_NAME=prometheus.yml2
 cat configs/prometheus.yml | docker config create ${PROMETHEUS_CONFIG_NAME} -
 
 docker stack deploy -c docker-compose.yml cluster_monitor
