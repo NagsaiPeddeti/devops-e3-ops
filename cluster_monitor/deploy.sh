95758398e3b167e01de471b2d3783c42b9de4000
NagsaiPeddeti <nagsai.peddeti@gmail.com>
2022-04-30 22:22:29 +0530

    added changes

--- cluster_monitor/deploy.sh
+++ cluster_monitor/deploy.sh
@@ -5,12 +5,12 @@ docker stack deploy -c docker-compose.yml cluster_monitor
 
 
 
-docker service create --name cadvisor -l prometheus-job=cadvisor \
-    --mode=global --publish target=8080,mode=host \
-     --network cluster_monitor_monitoring \
-    --mount type=bind,src=/var/run/docker.sock,dst=/var/run/docker.sock,ro \
-    --mount type=bind,src=/,dst=/rootfs,ro \
-    --mount type=bind,src=/var/run,dst=/var/run \
-    --mount type=bind,src=/sys,dst=/sys,ro \
-    --mount type=bind,src=/var/lib/docker,dst=/var/lib/docker,ro \
-    gcr.io/cadvisor/cadvisor:latest 
+#docker service create --name cadvisor -l prometheus-job=cadvisor \
+#    --mode=global --publish target=8080,mode=host \
+ #    --network cluster_monitor_monitoring \
+  #  --mount type=bind,src=/var/run/docker.sock,dst=/var/run/docker.sock,ro \
+ #   --mount type=bind,src=/,dst=/rootfs,ro \
+ #   --mount type=bind,src=/var/run,dst=/var/run \
+ #   --mount type=bind,src=/sys,dst=/sys,ro \
+ #   --mount type=bind,src=/var/lib/docker,dst=/var/lib/docker,ro \
+  #  gcr.io/cadvisor/cadvisor:latest 
