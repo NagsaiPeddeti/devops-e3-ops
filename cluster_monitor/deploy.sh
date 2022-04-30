d753de5ba75f99534d28a9d3f39f5225a732d4d5
NagsaiPeddeti <nagsai.peddeti@gmail.com>
2022-04-30 22:35:21 +0530

    updated prometheus yml to scrap cadvisor

--- cluster_monitor/deploy.sh
+++ cluster_monitor/deploy.sh
@@ -1,4 +1,4 @@
-export PROMETHEUS_CONFIG_NAME=prometheus.yml2
+export PROMETHEUS_CONFIG_NAME=prometheus.yml3
 cat configs/prometheus.yml | docker config create ${PROMETHEUS_CONFIG_NAME} -
 
 docker stack deploy -c docker-compose.yml cluster_monitor
