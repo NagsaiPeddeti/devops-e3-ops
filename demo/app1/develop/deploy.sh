e84a3c0d56625ca1c7de8709c6f5805fdaeef693
NagsaiPeddeti <nagsai.peddeti@gmail.com>
2022-05-04 08:45:01 +0530

    added changes

--- demo/app1/develop/deploy.sh
+++ demo/app1/develop/deploy.sh
@@ -2,7 +2,7 @@ VERSION=`jq -r .version code/package.json`
 echo $VERSION
 
 export IMAGE_NAME=localhost:5000/$1/$2/$3:$VERSION 
-export CONFIG_NAME=demo_app1_03052022-2.json
+export CONFIG_NAME=demo_app1_04052022.json
 
 cat configs/stage.json | docker config create ${CONFIG_NAME} -
 
